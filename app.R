7d4ffb36845c08c9ebbb66e9129c3c6f248b9bc1
Ahmed JOU <61011691+ahmedjoubest@users.noreply.github.com>
2021-03-26 10:39:44 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,39 +1,36 @@
-#testss
-#s
-#shiny manaer
-#install packages matnsach
 
-# Explique chaque famille de packages sert a quoi !!
-#
-# {plyr} package must be unloaded : bad interference with {dplyr} functions
-# detach("package:plyr", unload=TRUE)
+# 1.1 PACKAGES ----------------------------------------------------------
+
+#detach("package:plyr", unload=TRUE) must be unloaded : unwanted interference with {dplyr}
+
 library(scroller) # to install : remotes::install_github("lgnbhl/scroller")
-library(shinydashboard)
-library(pander)
+
 library(shiny)
+library(shinydashboard)
+
+library(shinyBS)
+library(shinycssloaders)
+library(shinyjs)
+library(shinyWidgets)
+
+library(lubridate)
 library(dplyr)
 library(highcharter)
 library(data.table)
-library(feather) # Faster reading than readRDS if we're not compromised by storage. More details in https://appsilon.com/fast-data-loading-from-files-to-r/
-library(shinyWidgets)
 library(DT)
-library(shinyBS)
-library(shinyjs)
-library(shinycssloaders)
-library(lubridate)
-# Reteb tes librairies m3a b3dyathom b tariqa shiha
+
+library(feather) # Faster than RDS if not compromised by storage. https://appsilon.com/fast-data-loading-from-files-to-r/
+
+# 1.2 Load Data + plotting functions  -----------------------------------
 
 source("helpers.R")
-
-# ALLL7777WWWAAAA =)
-# commenter had lqlawi fl app ;)
-#golih had nwita f mail anak z3ma wa3R f js donc sehlat 3lik lqadya
-
 Data <- read_feather("Data-Decathlon.feather")
+
+# 2.1 SERVER  -----------------------------------------------------------
 
 server <- function(input, output, session) {
   
-  # 2.0 Filtered data to visualize  ----
+  # 2.2 Reactive filtered Data  -----------------------------------------
   DT <- eventReactive(input$submit, ignoreNULL = FALSE,{
     subset(
       Data,
@@ -46,73 +43,91 @@ server <- function(input, output, session) {
     )
   })
   
+  # 2.3 Filter interdependence ------------------------------------------
   
+  ### {item} depends on <= {store} & {price range} ###
+  ### {Date}: independent filter                   ###
+  ####################################################
   
-  # 2.1 Filter interdependence ----
-  
-  ### Explanation of dependence between filters  ###
-  ### {price, item} depend on <= {store}         ###
-  ### {price} <=> {item} : interdependent        ###
-  ### {Date}: independent                        ###
-  ##################################################
-  
-  # Update item_names based on store_names with respecting the chosen price range 
+  # Update {item} based on {store} with respecting the chosen {price range}
   observeEvent(input$store_name,ignoreInit = TRUE, {
     item_names_update <- unique(Data$item_name[Data$store_name %in% input$store_name &
-                                                 Data$prices >= input$prices[1] & # to respect the chosen price range
-                                                 Data$prices <= input$prices[2] ])# to respect the chosen price range
+                                                 Data$prices >= input$prices[1] & # to respect the chosen {price range}
+                                                 Data$prices <= input$prices[2] ])# to respect the chosen {price range}
     updatePickerInput(session, "item_name", selected = item_names_update, choices = item_names_update)
   })
-
-  # Update item_names based on prices with respecting the chosen store_names
+  
+  # Update {item} based on {price range} with respecting the chosen {store}
   observeEvent(input$prices,ignoreInit = TRUE, {
-    item_names_update <- unique(Data$item_name[Data$store_name %in% input$store_name & # to respect chosen store_names
+    item_names_update <- unique(Data$item_name[Data$store_name %in% input$store_name & # to respect chosen {store}
                                                  Data$prices >= input$prices[1] & 
                                                  Data$prices <= input$prices[2] ])
     updatePickerInput(session, "item_name", selected = item_names_update, choices = item_names_update)
-    })
-  
-  output$heat_map_correlation <- renderHighchart({
-    Make_HC_Heatmap_Correlation(DT(), X = 'store_name',Y = 'item_name',
-                         intensity = input$intensity_1, #default input='the_transaction_id' for transaction occurrence
-                         transaction_type = input$transaction_type_1) #default input='sales'
   })
   
+  
+  # 2.4 Items/stores correlation heat map  ------------------------------
+  
+  output$heat_map_correlation <- renderHighchart({
+    Make_HC_Heatmap_Correlation(DT = DT(), X = 'store_name',Y = 'item_name',
+                                intensity = input$intensity_1, # default input='the_transaction_id' for transaction occurrence
+                                transaction_type = input$transaction_type_1 # default input='return'
+    ) 
+  })
+  
+  # 2.5 Chronological comparison heat map  -----------------------------
+  
   output$heat_map_chronologic <- renderHighchart({
-    Make_HC_Chronological_heatmap(DT(), 
-                                X = input$display,#default input = 'day'
-                                intensity = input$intensity_2,  #default input='the_transaction_id' for transaction occurrence
-                                transaction_type = input$transaction_type_2) #default input='sales' 
+    Make_HC_Chronological_heatmap(DT= DT(), 
+                                  X = input$display, # default input = 'day'
+                                  intensity = input$intensity_2,  # default input='the_transaction_id' for transaction occurrence
+                                  transaction_type = input$transaction_type_2) # default input='sale' 
+  })
+  
+  # 2.6 Stores comparison barplot  -------------------------------------
+  
+  output$Bar_plot_comparaison <- renderHighchart({
+    Make_HC_Barplot(DT = DT(), 
+                    X = "store_name",
+                    Y = input$barplot_y_axis, # default input='the_transaction_id' for transaction occurrence
+                    group = "tdt_type_detail",
+                    percent = input$percent, # default input = FALSE 
+                    horizontal = input$horizontal) # default input = FALSE 
   })
 }
 
 
-## 1. header ------------------------------
+# 3.1 HEADER -----------------------------------------------------------
 header <- 
   dashboardHeader(title = "Sales Analysis")
 
-## 2. sidebar ------------------------------
+# 3.2 SIDEBAR ----------------------------------------------------------
 sidebar <- 
+  
   dashboardSidebar(
+    
     sidebarMenu(
       id = "sidebar",
       menuItem("Main Dashboard", tabName = 'dashboard', icon = icon('dashboard')),
-      menuItem("Filtered data", tabName = 'Filtered Data?icon', icon = icon('table'))
-      
-      ),
+      menuItem("Filtered data", tabName = 'Filtered_Data', icon = icon('table'))
+    ),
     
     br(),
     
     h4(tags$u("Filter Data:"),align = "center"),
     h6(tags$u("note"),": please filter from top to bottom, hover filters fore more info.", style='margin-left: 1.4em; color:red;'),
+    
+    ## Stores filter
     tipify(pickerInput(
       inputId = "store_name",
       label = "Stores",
       choices = unique(Data$store_name),
       multiple = TRUE,
-      selected = unique(Data$store_name)[1:5],
+      selected = unique(Data$store_name)[1:6],
       options = pickerOptions(actionsBox = TRUE, liveSearch = TRUE, size = 5, dropdownAlignRight = T)
     ),"Dependence info: this filter acts on {Items} filter","right"),
+    
+    ## Price range filter
     tipify(sliderInput(
       inputId = "prices",
       label = "Range of item price in Euro",
@@ -120,14 +135,18 @@ sidebar <-
       max = round(max(Data$prices))+1, 
       value = c(1,500)
     ),"Dependence info: this filter acts on {Items} filter","right"),
+    
+    ## Items filter
     tipify(pickerInput(
       inputId = "item_name",
       label = "Items",
       choices = unique(Data$item_name),
-      selected = unique(Data$item_name)[1:5],
+      selected = unique(Data$item_name)[c(2,3,5,6,7,8,10,14,15)],
       multiple = TRUE,
       options = list(`actions-box` = TRUE,`live-search` = TRUE,size = 5)
     ),"Dependence info: this filter does NOT act on any filter!","right"),
+    
+    ## Date range filter
     tipify(dateRangeInput('date',
                           'Date range (2015-2019)',
                           start = min(Data$the_date_transaction),
@@ -137,36 +156,50 @@ sidebar <-
     br(),
     
     div(align = 'center',
+        
+        ## Filter button
         tipify(actionBttn(
           inputId = "submit",
           label = "Filter",
           style = "stretch", 
           color = "primary",
           icon = icon("sliders")
-          ),
-          "Apply filters",
-          placement = "top"
-        ))
+        ), "Apply filters", placement = "top"),
+        
+        br(),br(),br(),
+        
+        ## Scroll down button
+        a(tags$b("Scroll Down",style="color: black;"),
+          type="button", class = "btn btn-primary",
+          href = "#.down", icon("arrow-down",class = "arrow_down"))
     )
+  )
 
-## 3. body --------------------------------
+# 3.3 DASHBOARD BODY -------------------------------------------------
 body <- dashboardBody( 
   
+  ## Set scrolling options
+  scroller::use_scroller(animationLength = 265),
   
-  scroller::use_scroller(),
-  # styling bla bla comment
+  ## Import styling CSS file
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "css2.css") 
+    tags$link(rel = "stylesheet", type = "text/css", href = "css4.css") 
   ),
   
-  ## 3.1 Dashboard body --------------
+  # 3.3 DASHBOARD BODY  ----------------------------------------------
+  
   tabItems(
-    ## 3.1 Main dashboard ---------------------------------------------------------- 
     tabItem(tabName = 'dashboard',
             
-            # chronological evolution heatmap :
-            h2("Heatmap of chronological map"),
-            HTML("zbi"),
+            h3("Heatmap of chronological  this when you groupped by thus
+               and the beaut of heat and hor were i am in love with you 
+               bro etc. of this when you groupped by thus and the beaut 
+               of heat and hor were i am in love with you bro etc..",
+               style="margin-left: 0.65em;"),
+            
+            br(),
+            
+            # 3.4 chronological comparison heatmap box --------------
             
             box(title = HTML("<div class='w-h-t-circle'> </div> 
                      <b class='boxtitle' > Heatmap of chronogical evolution </b>"),
@@ -186,26 +219,30 @@ body <- dashboardBody(
                   ),
                   column(6,
                          radioGroupButtons("intensity_2", label = h5("Intensity variable"), status = "primary",
-                         choices=c("Total transaction" = "the_transaction_id",
-                                   "Total turnover" = "turnover",
-                                   "Total quantity" = "quantity"),
-                         justified =T,width = "100%", size = "xs", individual = TRUE
+                                           choices=c("Total transactions" = "the_transaction_id",
+                                                     "Total turnover" = "turnover",
+                                                     "Total quantity" = "quantity"),
+                                           selected = "turnover",
+                                           justified =T,width = "100%", size = "xs", individual = TRUE
                          )
                   ),
                   column(2,
                          tipify(radioGroupButtons("display", label = h5("Display by"), status = "primary",
-                         choices=c("Days"="day","Months"='month', "Quarters"='quarter'), justified =T,width = "100%",
-                         size = "xs", individual = TRUE
+                                                  choices=c("Days"="day","Months"='month', "Quarters"='quarter'), justified =T,width = "100%",
+                                                  size = "xs", individual = TRUE
                          ),"If aggregated by months or quarters : make sure you filter by a large period of time (more than one year) so that you can visualize the effect of different categories of month/quarter on the sales",
                          "bottom"
                          )
                   ),
                   column(1),
-                  ),
-
+                ),
+                
                 highchartOutput("heat_map_chronologic") %>%
                   withSpinner(color="#3C8DBC",type=4, size = 1.1)
                 ,width=12,status="primary", solidHeader = T),
+            
+            
+            
             
             # correlation heatmap :
             box(title = HTML("<div class='w-h-t-circle'> </div> 
@@ -217,7 +254,7 @@ body <- dashboardBody(
                                            status = "primary", choices=c("Sales" = "sale" ,"Returns" = "return"),
                                            justified =T,width = "100%", size = "xs", individual = TRUE,
                                            selected = 'return'
-                                           )
+                         )
                   ),
                   column(1),
                   column(6,
@@ -232,12 +269,45 @@ body <- dashboardBody(
                 ),
                 highchartOutput("heat_map_correlation") %>%
                   withSpinner(color="#3C8DBC",type=4, size = 1.1)
-                ,width=12,status="primary", solidHeader = T),            
+                ,width=12,status="primary", solidHeader = T),
+            
+            box(title = HTML("<div class='w-h-t-circle'> </div> 
+                            <b class='boxtitle' > Barplot comparaison of sales in
+                             different stores </b>"),
+                fluidRow(
+                  column(1),
+                  column(2,h5("Horizontal bars",style="margin-bottom: 1.15em;"),
+                         materialSwitch(
+                           inputId = "horizontal",
+                           value = FALSE,
+                           status = "primary",
+                         )
+                  ),
+                  column(2,h5("Show percentage",style="margin-bottom: 1.15em;"),
+                         materialSwitch(
+                           inputId = "percent",
+                           value = FALSE,
+                           status = "primary",
+                         )
+                  ),
+                  column(6,
+                         radioGroupButtons("barplot_y_axis", label = h5("Display by"), status = "primary",
+                                           choices=c("Number of transactions" = "the_transaction_id",
+                                                     "Total turnover" = "turnover",
+                                                     "Total quantity" = "quantity"),
+                                           justified =T,width = "100%", size = "xs", individual = TRUE
+                         )
+                  ),
+                  column(1)
+                ),
+                highchartOutput("Bar_plot_comparaison") %>%
+                  withSpinner(color="#3C8DBC",type=4, size = 1.1)
+                ,width=12,status="primary", solidHeader = T),
             
             # Button to scroll to the top (toggle sidebar)
-            fluidRow(a("Scroll to top ", type="button", class = "btn btn-primary",
-                       href = "#.sidebar-toggle", icon("arrow-up")),
-              align = "center")
+            div(a("Scroll to top ", type="button", class = "btn btn-primary down",
+                  href = "#.sidebar-toggle", icon("arrow-up")),
+                align = "center")
             
             
     )
@@ -249,7 +319,7 @@ body <- dashboardBody(
       })
      '))
 )
- 
+
 
 
 ## put UI together --------------------
